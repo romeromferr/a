c6ae3d2c8bf3f577aa88e684e9f8a5c5c0655216
romeromferr <romeromferr@gmail.com>
2024-09-03 22:13:50 -0300

    Create MrMenu2.js

---
+++ MrMenu2.js
@@ -0,0 +1,9 @@
+window.o2 = WebSocket.prototype.send;
+window.webSocket.prototype.send = function(args) 
+{
+    if(args.startsWith('42[4,21'))
+    {
+      console.log(args));
+    }
+    return window.o2.call(this,args);
+}
